bee6b7c05adfdc8c342a55324aaef61c4a2127a0
Mohamad al moualem <m.almouallem@gmail.com>
2018-10-02 11:18:40 +0200

    Fix passing vars

--- tasks.sh
+++ tasks.sh
@@ -44,7 +44,7 @@ test() {
 
 build() {
     dep_insure
-	env GOOS=$(TARGET) GOARCH=$(ARCH) go build -ldflags="-s -w" -o bin/"$entrypoint" "$entrypointPath"
+	env GOOS=$TARGET GOARCH=$ARCH go build -ldflags="-s -w" -o bin/"$entrypoint" "$entrypointPath"
     print success "create a build"
     zip bin/"$entrypoint".zip bin/"$entrypoint" > executable
     print success "zip build executable"
