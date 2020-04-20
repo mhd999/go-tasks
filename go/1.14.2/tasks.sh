7e059cac6d9a782c0865c9fb7f0fc72474802ca6
Mohamad al moualem <m.almouallem@gmail.com>
2020-04-20 16:55:22 +0200

    Feat: go 1.14

---
+++ go/1.14.2/tasks.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+set -e
+
+print() {
+    local GREEN='\033[1;32m'
+    local YELLOW='\033[1;33m'
+    local RED='\033[0;31m'
+    local NC='\033[0m'
+    local BOLD='\e[1m'
+    local REGULAR='\e[0m'
+    case "$1" in
+        'failure' ) echo -e "${RED}✗ $2${NC}" ;;
+        'success' ) echo -e "${GREEN}√ $2${NC}" ;;
+        'warning' ) echo -e "${YELLOW}⚠ $2${NC}" ;;
+        'header'  ) echo -e "${BOLD}$2${REGULAR}" ;;
+    esac
+}
+
+t="linux"
+a="amd64"
+TARGET="${TARGET:-$t}"
+ARCH="${ARCH:-$a}"
+
+
+export_env_vars() {
+    for s in $(echo $envVars | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
+        export $s
+    done
+    print success "export environment variables"
+}
+
+setup() {
+    export DIR="$PWD"
+    JQ=/usr/bin/jq
+    curl https://stedolan.github.io/jq/download/linux64/jq > $JQ && chmod +x $JQ
+    ls -la $JQ
+    print success "install jq"
+    export_env_vars
+}
+
+test() {
+    go test ./... --cover
+    print success "tests passed"
+}
+
+build() {
+	env GOOS=$TARGET GOARCH=$ARCH go build -ldflags="-s -w" -o ${DIR}/executable/"$entrypoint" "$entrypointPath"
+    print success "create a build"
+}
+
+main() {
+    if [ -z "$command" ]; then
+        echo "Command is a required parameter and must be set."
+        exit 1
+    fi
+    if [ -z "$directory" ]; then
+        echo "No directory provided. Please set the parameter."
+        exit 1
+    fi
+
+    setup
+
+    export GOPATH=$PWD
+
+    mkdir -p src/github.com/$organization/$project
+    cp -R ./source/* src/github.com/$organization/$project/.
+    cd src/github.com/$organization/$project/$directory
+
+    print header "Current directory: $directory"
+
+    case "$command" in
+        'test'        ) test ;;
+        'build'       ) build ;;
+        *             ) echo "Command not supported: $command" && exit 1;;
+    esac
+}
+
+main
